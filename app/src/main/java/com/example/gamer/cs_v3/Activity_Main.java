b9bc283fef2028a6451420307412254eed2b749f
MadsChris92 <madschris888@gmail.com>
2018-04-27 22:57:16 +0200

    Volley and shizz

--- app/src/main/java/com/example/gamer/cs_v3/Activity_Main.java
+++ app/src/main/java/com/example/gamer/cs_v3/Activity_Main.java
@@ -1,13 +1,57 @@
 package com.example.gamer.cs_v3;
 
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 public class Activity_Main extends AppCompatActivity {
+    LinearLayout content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__main);
+
+        // UI
+        content = findViewById(R.id.scroll_content);
+
+        // Setup
+        final TextView textView = new TextView(this);
+        textView.setText("Test");
+        content.addView(textView);
+
+        String url = "https://api.pandascore.co/videogames?token=PUbBYoQNl8UBcjZ0nvOHSPbJEGMEHtV75-437VksZ2bsKdNOb34";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(final JSONObject response) {
+                        textView.setText("Response: " + response.toString());
+                        Button b = new Button(getBaseContext());
+                        content.addView(b);
+                        b.setOnClickListener(e -> textView.setText(response.toString()));
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
 }
