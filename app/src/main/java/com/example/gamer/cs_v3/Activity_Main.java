73382e194f164e88a009d308c11c5f16dc61bb94
MadsChris92 <madschris888@gmail.com>
2018-04-28 20:56:36 +0200

    volley jsonarray works

--- app/src/main/java/com/example/gamer/cs_v3/Activity_Main.java
+++ app/src/main/java/com/example/gamer/cs_v3/Activity_Main.java
@@ -3,15 +3,18 @@ package com.example.gamer.cs_v3;
 import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Activity_Main extends AppCompatActivity {
@@ -32,26 +35,43 @@ public class Activity_Main extends AppCompatActivity {
 
         String url = "https://api.pandascore.co/videogames?token=PUbBYoQNl8UBcjZ0nvOHSPbJEGMEHtV75-437VksZ2bsKdNOb34";
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                response -> {
+                    // Do something with response
+                    //mTextView.setText(response.toString());
 
-                    @Override
-                    public void onResponse(final JSONObject response) {
-                        textView.setText("Response: " + response.toString());
-                        Button b = new Button(getBaseContext());
-                        content.addView(b);
-                        b.setOnClickListener(e -> textView.setText(response.toString()));
+                    // Process the JSON
+                    try{
+                        // Loop through the array elements
+                        for(int i=0;i<response.length();i++){
+                            // Get current json object
+                            JSONObject game = response.getJSONObject(i);
+
+                            // Get the current student (json object) data
+                            int id = game.getInt("id");
+                            String name = game.getString("name");
+                            textView.append(id + " " + name + "\n");
+                            // Display the formatted json data in text view
+
+                        }
+
+                        textView.append(" " + response.length());
+                    }catch (JSONException e){
+                        e.printStackTrace();
                     }
-                }, new Response.ErrorListener() {
+                },
+                error -> {
+                    // Do something when error occurred
+                    Log.e("Response", "Error");
+                }
+        );
 
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO: Handle error
-
-                    }
-                });
 
         // Access the RequestQueue through your singleton class.
-        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+        MySingleton.getInstance(this).addToRequestQueue(jsonArrayRequest);
+
     }
 }
